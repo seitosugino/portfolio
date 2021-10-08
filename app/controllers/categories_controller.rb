732180cef09614539681c1e612288268a5fb0700
seitosugino <0jb4f61k3853v2f@gmail.com>
2021-10-08 05:31:42 +0000

    マイページ修正

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,7 +2,7 @@ class CategoriesController < ApplicationController
   def show
     @categories = Category.all
     @category = Category.find(params[:id])
-    @posts = Post.search(params[:search]).page(params[:page]).per(8)
+    @posts = @category.posts.search(params[:search]).page(params[:page]).per(8)
     @amount = @category.posts.count
   end
   
