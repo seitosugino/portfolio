1ea9b8fc65c64845ecb9e0a94bca71554efe0334
seitosugino <0jb4f61k3853v2f@gmail.com>
2021-10-13 10:35:10 +0000

    DM、グループチャット非同期

--- app/controllers/group_posts_controller.rb
+++ app/controllers/group_posts_controller.rb
@@ -2,9 +2,11 @@ class GroupPostsController < ApplicationController
 
   def create
     @group = Group.find_by(id: params[:group_id])
+    @group_posts = GroupPost.group(params[:id])
     @group_post = GroupPost.new(group_post_params)
     @group_post.customer_id = current_customer.id
-    if @group_post.save!
+    if @group_post.save
+    else
       redirect_to group_path(@group_post.group_id)
     end
   end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,18 @@
 class MessagesController < ApplicationController
   before_action :authenticate_customer!, only: [:create]
+  before_action :set_room, only: [:create]
   
   def create
+    @messages = @room.messages
     if Entry.where(customer_id: current_customer.id, room_id: params[:message][:room_id]).present?
-      @message = Message.create!(params.require(:message).permit(:customer_id, :content, :room_id).merge(customer_id: current_customer.id))
-      redirect_to "/rooms/#{@message.room_id}"
+      @message = Message.create(params.require(:message).permit(:customer_id, :content, :room_id).merge(customer_id: current_customer.id))
     else
       redirect_back(fallback_location: root_path)
     end
   end
+  
+  def set_room
+    @room = Room.find(params[:message][:room_id])
+  end
+
 end
