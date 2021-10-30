0db427a6abec576b8dfcb6b7ef836b36e5aaaef1
seitosugino <0jb4f61k3853v2f@gmail.com>
2021-10-30 14:30:02 +0000

    rspec

--- spec/factories/customer.rb
+++ spec/factories/customer.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :customer do
     sequence(:name) { |n| "TEST_NAME#{n}"}
-    sequence(:email) { |n| "TEST#{n}@example.com"}
+    sequence(:email) { |n| "#{n}@example.com"}
     sequence(:password) { |n| "123456"}
   end
 end
